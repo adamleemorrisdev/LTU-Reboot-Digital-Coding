0e44231c32654b17a2785bb5b1939f1f887161fa
Rinzer1 <csxa_@outlook.com>
2021-11-11 21:03:31 +0000

    completing javascript

---
+++ module-3/task/start/scripts/main.js
@@ -0,0 +1,21 @@
+console.log('hello');
+
+// Single line comment
+
+/*---
+
+Multi-line comment
+
+---*/ 
+
+let productname = 'mangoes';
+let price = '10.00';
+let quantity = 3;
+let inStock = true;
+let discountAmount= 0;
+
+console.log(productname);
+console.log(price)
+console.log(quantity)
+console.log(inStock)
+console.log(discountAmount)
