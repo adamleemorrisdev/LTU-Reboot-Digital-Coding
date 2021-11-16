bd18661b4068c6fc0a1e84be49aec8567124b522
Rinzer1 <csxa_@outlook.com>
2021-11-16 21:03:03 +0000

    completing tasks from javascript session 2

--- module-3/task/start/scripts/main.js
+++ module-3/task/start/scripts/main.js
@@ -8,14 +8,46 @@ Multi-line comment
 
 ---*/ 
 
-let productname = 'mangoes';
-let price = '10.00';
-let quantity = 3;
+let productName = 'Macbook';
+let productPrice = '10.00'; // unless set as a constant, this can be overwritten
+let productQuantity = 3;
 let inStock = true;
-let discountAmount= 0;
+let discountAmount= 2;
 
-console.log(productname);
-console.log(price)
-console.log(quantity)
-console.log(inStock)
-console.log(discountAmount)
+console.log(productName);
+console.log(productPrice);
+console.log(productQuantity);
+console.log(inStock);
+console.log(discountAmount);
+
+function showProductName(){
+    console.log(productName);
+}
+showProductName();
+
+function totalPrice(productPrice,productQuantity){
+    console.log(productPrice * productQuantity)
+}
+
+totalPrice(productPrice,productQuantity);
+
+function squareNumber(number=0){
+    let sum = number * number;
+    return sum;
+}
+    var square = number => number * number;
+
+    let squaredNumber = squareNumber(3);
+    console.log(squaredNumber);
+
+    let squared = square(3);
+    console.log(squared);
+
+function calculateDiscount(){
+    let sum = productPrice * productQuantity;
+    if(productQuantity > 1){
+        sum = sum - discountAmount;
+    }
+    console.log(sum);
+}
+calculateDiscount();        
