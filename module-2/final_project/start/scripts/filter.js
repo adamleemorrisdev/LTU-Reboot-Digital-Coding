8ed51de16302230a380a489ef525076ebcd545f6
Rinzer1 <csxa_@outlook.com>
2021-12-02 21:08:42 +0000

    adding image show/hide function

--- module-2/final_project/start/scripts/filter.js
+++ module-2/final_project/start/scripts/filter.js
@@ -2,8 +2,12 @@ console.log("Test");
 
 let buttons = document.querySelectorAll('.animalFilter');
 let images = document.querySelectorAll('.imageFilter');
+let textSearchInput = document.querySelector('.textSearchInput');
+let searchString;
+let animal = 'all';
 console.log(buttons);
 console.log(images);
+console.log(textSearchInput);
 
 buttons.forEach(function(button){
     // console.log(button);
@@ -12,17 +16,31 @@ buttons.forEach(function(button){
         let clickedItem = event.target;
         let animal = clickedItem.getAttribute('animal');
         // console.log(animal);
-        images.forEach(function(image){
+        images.forEach(function(image){ // creating and using
             image.parentElement.classList.add('hide');
             if(image.classList.contains(animal) || animal == 'all'){
                 console.log(image);
                 if(image.parentElement.classList.contains('hide'));
-                image.parentElement.classList.remove('hide');
+                    image.parentElement.classList.remove('hide');
             }
         });
     });
 });
 
-// buttons.forEach(function(button){
-//     console.log(button);
-// });
+textSearchInput.addEventListener('keyup',function(event){
+    let searchString = event.target.value;
+    console.log(searchString);
+    images.forEach(function(image){
+        let captionElement = image.parentElement;
+        let caption = captionElement.textContent.trim();
+        image.parentElement.classList.add('hide');
+        if(caption.indexOf(searchString)!== -1){
+            console.log(caption.indexOf(searchString));
+            // show current image
+            if(image.parentElement.classList.contains('hide')){
+                image.parentElement.classList.remove('hide');
+            }
+        }
+    });
+});
+
